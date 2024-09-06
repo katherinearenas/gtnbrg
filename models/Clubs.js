66f8d4414747d048cbecf5df16921e958e362e26
Patrick Hennessey <pjhennessey89@gmail.com>
2024-09-05 19:27:30 -0500

    Sign up page rendering correctly

--- models/Clubs.js
+++ models/Clubs.js
@@ -1,10 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create our Traveller model
 class Club extends Model {}
 
-// create fields/columns for Traveller model
 Club.init(
   {
     id: {
@@ -18,7 +16,6 @@ Club.init(
       allowNull: false
     },
     host: {
-
       type: DataTypes.INTEGER,
       allowNull: false,
       unique: true,
@@ -26,7 +23,6 @@ Club.init(
         model: 'member',
         key: 'id',
         unique: true
-
       }
     },
     description: {
@@ -35,10 +31,9 @@ Club.init(
     },
 
     members_list: {
-
       type: DataTypes.INTEGER,
       allowNull: true
-    },
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -2,31 +2,21 @@ const Book = require('./Books');
 const Club = require('./Clubs');
 const Member = require('./Members');
 const Library = require('./Libraries');
-const Memberlist = require('./Memberlists')
+const Memberlist = require('./Memberlists');
 
 Book.belongsToMany(Member, {
-
-  // Define the third table needed to store the foreign keys
   through: {
     model: Library,
     unique: false
   },
-
-  // Define an alias for when data is retrieved
-
   as: 'collection'
 });
 
 Book.belongsToMany(Club, {
-
-  // Define the third table needed to store the foreign keys
   through: {
     model: Library,
     unique: false
   },
-
-  // Define an alias for when data is retrieved
-
   as: 'reading_list'
 });
 
@@ -35,17 +25,19 @@ Club.belongsToMany(Member, {
     model: Memberlist,
     unique: false
   },
-  as: 'member_list'
+  as: 'member_list',
+  foreignKey: 'club_id',
+  otherKey: 'user_id'
 });
 
-
 Member.belongsToMany(Club, {
-
   through: {
     model: Memberlist,
     unique: false
   },
-  as: 'membership'
+  as: 'membership',
+  foreignKey: 'user_id',
+  otherKey: 'club_id'
 });
 
 module.exports = { Book, Club, Member, Library };

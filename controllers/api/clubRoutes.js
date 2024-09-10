8756ef1efad5c75b68ebbfc022c4d7350dae16a7
Patrick Hennessey <pjhennessey89@gmail.com>
2024-09-10 17:06:45 -0500

    Can set current book for club

--- controllers/api/clubRoutes.js
+++ controllers/api/clubRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Club, Memberlist, Member, Library } = require('../../models');
+const { Club, Memberlist, Member, Library, Book } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
@@ -28,16 +28,35 @@ router.get('/new', (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const club = await Club.findByPk(req.params.id, {
-      include: [{
-        model: Member,
-        as: 'members'
-      }]
+      include: [
+        {
+          model: Member,
+          as: 'members',
+          attributes: ['name']
+        },
+        {
+          model: Member,
+          as: 'hostDetails',
+          attributes: ['name']
+        },
+        {
+          model: Book,
+          as: 'books_in_club',
+          through: {
+            attributes: [],
+          },
+        },
+      ],
     });
     
     if (!club) {
       res.status(404).json({ message: 'No club found with this id!' });
       return;
     }
+
+    const currentBook = club.books_in_club.length ? club.books_in_club[0] : null;
+
+    const isHost = club.host === req.session.memberId;
 
     const isMember = await Memberlist.findOne({
       where: {
@@ -49,7 +68,10 @@ router.get('/:id', async (req, res) => {
     res.render('club', { 
       club: club.toJSON(),
       isMember: !!isMember,
-      clubMembers: club.members
+      clubMembers: club.members,
+      currentBook,
+      clubBooks: club.books_in_club,
+      isHost
      });
 
   } catch (err) {
@@ -101,6 +123,41 @@ router.post('/new', async (req, res) => {
     res.status(500).send('Error creating club');
   }
 });
+
+router.post('/:id/setBook', async (req, res) => {
+  try {
+    const { bookName, authorName, genre, description } = req.body;
+
+    const club = await Club.findByPk(req.params.id);
+
+    if (club.host !== req.session.memberId) {
+      return res.status(403).json({
+        message: 'Only host can choose book.'
+      });
+    }
+
+    const [book, created] = await Book.findOrCreate({
+      where: { name: bookName},
+      defaults: {
+        author: authorName,
+        genre,
+        description,
+      }
+    });
+
+    await Library.create({
+      club_id: club.id,
+      book_id: book.id,
+    })
+
+    res.redirect(`/api/clubs/${club.id}`)
+  } catch (error) {
+    console.error("Error setting book:", error);
+    res.status(500).json({
+      message: 'Failed to set book'
+    });
+  }
+})
 
 // DELETE a club
 router.delete('/:id', async (req, res) => {

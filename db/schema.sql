3e39cfbda76b3013e2eb9c98d8233b4d32d6f8cb
Wrandy1 <cjyoung0420@gmail.com>
2024-09-09 19:01:35 -0500

    merged

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 -- SQLBook: Code
-=======
->>>>>>> 23c23b5e66d0c53d4430bde8d3d5187c5c0fbf30
--- Active: 1723505729037@@127.0.0.1@5432@bookclub_db
--- SQLBook: Code
+
 
 -- Active: 1723505729037@@127.0.0.1@5432@bookclub_db
 -- SQLBook: Code
